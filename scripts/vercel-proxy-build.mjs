aea2a2b7aab94b9af62b673d18396a51e6952e3d
Tacyanne Ribeiro <tacytpr@gmail.com>
2026-07-24 17:27:13 -0300

    Remover arquivo de proxy não utilizado

--- scripts/vercel-proxy-build.mjs
+++
@@ -1,7 +0,0 @@
-import { mkdir, writeFile } from "node:fs/promises";
-
-await mkdir(".vercel-static", { recursive: true });
-await writeFile(
-  ".vercel-static/index.html",
-  "<!doctype html><html><head><meta charset=\"utf-8\"><title>Pino Forte</title></head><body></body></html>\n",
-);
